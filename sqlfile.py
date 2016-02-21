9383a4730f1d1efd91c680effc117a830a03f72a
juanrodriguez58 <juanrodriguez58@gmx.us>
2016-02-21 12:45:56 +0100

    sql

--- sqlfile.py
+++ sqlfile.py
@@ -49,52 +49,51 @@ def borraCount(dire):
         return("Error al borrar el registro")
 
 def actualizaCount1(dire,c1):
-    sqlOrder = 'UPDATE cicle SET count1 = {0} WHERE  direccion = {1}'.format(c1, dire)
+    sqlOrder = 'UPDATE cicle SET count1 = ? WHERE  direccion = ?'
     try:
         conn = sqlite3.connect('enlaces.sqlite3')
         cur = conn.cursor()
-        cur.execute(sqlOrder)
+        cur.execute(sqlOrder(c1, dire))
         conn.commit()
         return("Registro actualizado")
     except:
         return("Error al actualizar el registro")
 
 def actualizaCount2(dire,c2):
-    sqlOrder = 'UPDATE cicle SET count2 = {0} WHERE  direccion = {1}'.format(c2, dire)
+    sqlOrder = 'UPDATE cicle SET count2 = ? WHERE  direccion = ?'
     try:
         conn = sqlite3.connect('enlaces.sqlite3')
         cur = conn.cursor()
-        cur.execute(sqlOrder)
+        cur.execute(sqlOrder, (c2, dire))
         conn.commit()
         return("Registro actualizado")
     except:
         return("Error al actualizar el registro")
 
 def consultaCount1(dire):
-    lista = []
-    sqlOrder = 'SELECT count1 FROM cicle WHERE  direccion = {0}'.format(dire)
+    count=0
+    sqlOrder = 'SELECT * FROM cicle WHERE direccion = ?'
     try:
         conn = sqlite3.connect('enlaces.sqlite3')
         cur = conn.cursor()
-        cur.execute(sqlOrder)
-        for fila in cur:
-            lista.append(fila)
+        cur.execute(sqlOrder, (dire, ))
+        count = cur.fetchone()[2]
+        return count
     except:
-        lista.append(('No records found','',0,0))
-    return lista
+        print("Error en la lectura1")
+    return count
 
 def consultaCount2(dire):
-    sqlOrder = 'SELECT count2 FROM cicle WHERE  direccion = {0}'.format(dire)
+    sqlOrder = 'SELECT * FROM cicle WHERE direccion = ?'
     count = 0
     try:
         print("Busco contador 2:",  sqlOrder)
         conn = sqlite3.connect('enlaces.sqlite3')
         cur = conn.cursor()
-        cur.execute(sqlOrder)
-        for fila in cur:
-            count = int(fila)
-            print ("Contador 2:",  count )
+        cur.execute(sqlOrder, (dire, ))
+        count = cur.fetchone()[3]
     except:
+            print("Error en la lectura2")
             count = 0
     return count
 
